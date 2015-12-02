ba9e13f24279e5b21fe5a2609a1ae28bd57d4126
Lucas Jones <lucasarchboldjones@gmail.com>
2015-12-02 12:31:49 +0000

    Added Liam's code

--- Coursework/Coursework.cpp
+++ Coursework/Coursework.cpp
@@ -2,26 +2,41 @@
 #include <string>
 
 using namespace std;
-//Work dammit
+
 
 class Person
 {
 
 protected:
-	string m_FirstName, m_LastName;
+	string m_Name, m_Surname;
 
 public:
 	Person() {}
-	Person(const string& firstName, const string& lastName)
-	{}
-
-	string get_name() const
+	Person(string f, string s) : m_Name(f), m_Surname(s)
 	{
-		return m_FirstName;
+		m_Name = "NoName";
+		m_Surname = "NoSurname";
+		set_name(f);
+		set_surname(s);
 	}
-	string get_surname() const
+
+	void set_name(string f)
 	{
-		return m_LastName;
+		m_Name = f;
+	}
+
+	void set_surname(string s)
+	{
+		m_Surname = s;
+	}
+
+	string get_name()
+	{
+		return m_Name;
+	}
+	string get_surname()
+	{
+		return m_Surname;
 	}
 };
 
@@ -32,9 +47,20 @@ protected:
 	string m_telephone;
 
 public:
-	Person_with_telephone() {}
-	Person_with_telephone(const string& telephone) : m_telephone(telephone)
-	{}
+	Person_with_telephone(string telephone)
+	{
+		set_telephone(telephone);
+	}
+
+	void set_telephone(string telephone)
+	{
+		m_telephone = telephone;
+	}
+
+	string get_telephone()
+	{
+		return m_telephone;
+	}
 
 	bool has_telephone_p()
 	{
@@ -47,42 +73,34 @@ public:
 
 		else
 		{
-			cout << "Your number is: " << m_telephone << endl;
+			cout << "Your phone number is now registered"<< endl;
 			return true;
 		}
 
 	}
 
-	string get_telephone() const
-	{
-		return m_telephone;
-	}
-
-	string set_telephone()
-	{
-		cout << "Input telephone number: " << endl;
-		cin >> m_telephone;
-	}
-
-
-
 };
 
 int main()
 {
-	string f, l, ph;
+	string f, s, t;
 
-	cout << "Enter fist name: ";
-	cin >> f;
-	cout << "Enter Last name: ";
-	cin >> l;
-	cout << "Enter telephone number: ";
-	//cin >> ph;
-	//Person p(f, l);
-	Person_with_telephone pwt(/*f, l,*/ ph);
-	pwt.get_telephone();
-	//cout << "Your name is: " << p.get_name() << " " << p.get_surname() << endl;
-	//cout << "Has telephone? " << pwt.has_telephone_p() << endl << " Your number is: " << pwt.get_telephone() << endl;
+	Person person("NoName","NoSurname");
+	cout << "Person Created" << endl;
+	cout << person.get_name() << " " << person.get_surname() << endl << endl;
+
+	cout << "Type your name: ";
+	cin >> f, person.set_name(f);
+
+	cout << "Type your Surname: ";
+	cin >> s, person.set_surname(s);
+
+	cout << "Your name is: " << person.get_name() << " " << person.get_surname() << endl << endl;
+
+	Person_with_telephone pwt("");
+	cout << pwt.has_telephone_p() << endl << endl << "Type Phonenumber: ";
+	cin >> t, pwt.set_telephone(t);
+	cout << pwt.has_telephone_p() << endl << endl << "Your Phonenumber is: " << pwt.get_telephone() << endl;
 
 	return 0;
 }
