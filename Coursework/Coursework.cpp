01d5a04f7721e54dd13ae2620a6b59e6c27fb745
AngryHobbit <liam_301@hotmail.co.uk>
2015-12-03 19:50:47 +0000

    Could break everything

--- Coursework/Coursework.cpp
+++ Coursework/Coursework.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <sstream>
 
 //Allows use of basic C++ functions
 using namespace std;
@@ -9,10 +10,10 @@ class Person
 {
 
 protected:
-	string m_Name = "NoName", m_Surname = "NoSurname", m_Telephone = "NoNumber", m_Email = "NoEmail";
+	string m_Name, m_Surname, m_Telephone, m_Email;
 
 public:
-	Person() {}
+	Person() { string m_Name = "NoName", m_Surname = "NoSurname", m_Telephone = "NoNumber", m_Email = "NoEmail"; }
 	Person(string &s, string &f)
 	{
 		set_surname(s);
@@ -84,6 +85,9 @@ public:
 
 	}
 
+	friend ostream &operator<<(ostream &output, Person &p);
+	friend istream &operator>>(istream &input, Person &p);
+
 };
 
 class Person_with_telephone : public virtual Person
@@ -142,41 +146,19 @@ public:
 	}
 };
 
-ostream& operator<<(ostream &os, Person &p)
+ostream& operator<<(ostream &output, Person &p)
 {
-	if (p.has_email_p() && p.has_telephone_p())
-	{
-		cout << "<person" << " S " << p.get_surname() << " F " << p.get_name() << " T " << p.get_telephone() << " E " << p.get_email()  << " >";
-	}
-	else if (p.has_email_p())
-	{
-		cout << "<person" << " S " << p.get_surname() << " F " << p.get_name() << " E " << p.get_email() << " >";
-	}
-	else if (p.has_telephone_p())
-	{
-		cout << "<person" << " S " << p.get_surname() << " F " << p.get_name() << " T " << p.get_telephone() << " >";
-	}
-	else
-	{
-		cout << "<person" << " S " << p.get_surname() << " F " << p.get_name() << " >";
-	}
-	return os;
+	cout << "<person" << " S " << p.get_surname() << " N " << p.get_name() << " T " << p.get_telephone() << " E " << p.get_email()  << " >";
+	return output;
 }
 
-//istream& operator>>(istream &is, Person &p) {
-//	string opening, s, surname, f, forename, closing;
-//
-//	if (
-//		(is >> opening >> s >> surname >> f >> forename >> closing)
-//		&&
-//		((opening == "<person") && (s == "S") && (f == "F") && (closing == ">"))
-//		)
-//	{
-//		p = Person(surname, forename);
-//	}
-//	return is;
-//}
-//
+istream& operator>>(istream &input, Person &p) 
+{
+	input >> p.set_surname >> p.set_name;
+	//put >> p.set_name;
+	return input;
+}
+
 //istream& operator>>(istream &is, Person_with_email &p) {
 //	string opening, s, surname, f, forename, e, email, closing;
 //
@@ -204,7 +186,7 @@ ostream& operator<<(ostream &os, Person &p)
 //	return is;
 //}
 //
-//istream& operator>>(istream &is, Person_with_telephone_email &p) {
+//istream& operator>>(istream &is, Person_with_telephone_and_email &p) {
 //	string opening, s, surname, f, forename, t, telephone, e, email, closing;
 //
 //	if (
@@ -217,7 +199,7 @@ ostream& operator<<(ostream &os, Person &p)
 //			(closing == ">"))
 //		)
 //	{
-//		p = Person_with_telephone_email(surname, forename, telephone, email);
+//		p = Person_with_telephone_and_email(surname, forename, telephone, email);
 //	}
 //	return is;
 //}
@@ -240,7 +222,7 @@ ostream& operator<<(ostream &os, Person &p)
 //	return person_pointer;
 //}
 //
-//Person_with_telephone_email* make_person_telephone_email(istream &stream) {
+//Person_with_telephone_and_email* make_person_telephone_email(istream &stream) {
 //	Person_with_telephone_email *person_pointer = new Person_with_telephone_email;
 //	stream >> *person_pointer;
 //	return person_pointer;
@@ -283,53 +265,14 @@ ostream& operator<<(ostream &os, Person &p)
 
 int main()
 {
-	string s, f, t, e;
+	cout << "Enter surname and name" << endl;
 
-	//Creates Person object and after input returns name and surname
-	Person person(s,f);
-	cout << "Person Created" << endl;
-	cout << person.get_name() << " " << person.get_surname() << endl << endl;
+	Person person;
+	cin >> person;
+	cout << person;
 
-	cout << "Type your name: ";
-	getline(cin, f), person.set_name(f);
 
-	cout << "Type your Surname: ";
-	getline(cin, s), person.set_surname(s);
 
-	cout << "Your name is: " << person.get_name() << " " << person.get_surname() << endl << endl;
-
-	//Creates Person with telephone object
-	Person_with_telephone pwt(s,f,t);
-	cout << "Person with telephone Created" << endl;
-	cout << pwt.has_telephone_p() << endl << endl << "Type Phonenumber: ";
-	getline(cin, t), pwt.set_telephone(t); //User inputs value of telephone number into t and pwt.set_telephone takes that as a parameter
-	cout << pwt.has_telephone_p() << endl << endl << "Your Phonenumber is: " << pwt.get_telephone() << endl;
-
-	cout << "Type your name: ";
-	getline(cin, f), pwt.set_name(f); //User inputs value of name into f and pwt.set_name takes that as a parameter
-
-	cout << "Type your surname: ";
-	getline(cin, s), pwt.set_surname(s); //User inputs value of surname into s and pwt.set_surname takes that as a parameter
-
-	//Print name and surname using the methods from Person_with_telephone
-	cout << "Your name is: " << pwt.get_name() << " " << pwt.get_surname();
-	cout << "your number is: " << pwt.get_telephone() << endl << endl;
-
-	//Creates Person with email object
-	Person_with_email email(s,f,e);
-	cout << email.has_email_p() << endl << endl << "Type email: ";
-	getline(cin, e), email.set_email(e);
-	cout << email.has_email_p() << endl << endl << "Your email is: " << email.get_email() << endl << endl;
-
-	//Creates Person with telephone
-	Person_with_telephone_and_email pwtae(s,f,t,e);
-	cout << pwtae.has_telephone_p() << endl << endl << "Type Phonenumber: ";
-	getline(cin, t), pwtae.set_telephone(t); //User inputs value of telephone number into t and pwtae.set_telephone takes that as a parameter
-	cout << pwtae.has_telephone_p() << endl << endl << "Your Phonenumber is: " << pwtae.get_telephone() << endl; //Boolean function runs and value of telephone number is printed
-
-	cout << pwtae.has_email_p() << endl << endl << "Type email: ";
-	getline(cin, e), pwtae.set_email(e);
-	cout << pwtae.has_email_p() << endl << endl << "Your email is: " << pwtae.get_email() << endl;
 
 	return 0;
 }
